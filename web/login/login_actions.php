7f59d90278f00be60303fe30e5a66a0b9525759d
itamm15 <osinski.mateusz15@gmail.com>
2025-01-25 20:44:07 +0100

    lekarz login

--- web/login/login_actions.php
+++ web/login/login_actions.php
@@ -2,6 +2,7 @@
   require_once("../../config.php");
 
   const ADMIN_EMAIL = "admin@przychodnia.com";
+  const PRZYCHODNIA_DOMAIN = "przychodnia.com";
   const ADMIN_DATA_FILE = "../../admin_data.txt";
 
   $email = $_POST['email'] ?? '';
@@ -10,6 +11,7 @@
 
   if ($action == 'zarejestruj') header('Location: ../register/register.php');
   if ($action == 'zaloguj' && $email == ADMIN_EMAIL) login_admin($password);
+  if ($action == 'zaloguj' && explode('@', $email)[1] == PRZYCHODNIA_DOMAIN) login_lekarz($email, $password);
   if ($action == 'zaloguj') login_user($email, $password);
 
   function login_admin($password) {
@@ -24,6 +26,25 @@
 
     // TODO:: wpp dodaj errory
     fclose($admin_data_file);
+  }
+
+  function login_lekarz($email, $password) {
+    $conn = get_conn();
+    $lekarz_query = "SELECT * FROM lekarze WHERE email = '$email'";
+    $result = mysqli_query($conn, $lekarz_query);
+
+    if (mysqli_num_rows($result) === 1) {
+      $lekarz = mysqli_fetch_assoc($result);
+
+      if (password_verify($password, $lekarz["haslo"])) {
+        start_session($lekarz['imie'], $lekarz['nazwisko'], $email, "lekarz");
+        header("Location: ../index/index.php");
+        exit;
+      }
+    }
+
+    // TODO: wpp dodaj errory
+    close_conn($conn);
   }
 
   function login_user($email, $password) {
