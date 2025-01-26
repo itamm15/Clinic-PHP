9484ceb7d6f38b3079ade6ef35823582d75828f1
itamm15 <osinski.mateusz15@gmail.com>
2025-01-26 11:41:09 +0100

    validate admin password

--- web/profil/profil_actions.php
+++ web/profil/profil_actions.php
@@ -1,4 +1,6 @@
 <?php 
+  require("../../shared_validations/valdiate_user.php");
+
   const ADMIN_DATA_FILE = "../../admin_data.txt";
   $is_change_password_action = $_POST["change_admin_password"] ?? '';
 
@@ -19,12 +21,16 @@
   }
 
   function change_admin_password($new_password) {
-    $admin_data_file = fopen(ADMIN_DATA_FILE, "w") or die("Nie udało się otworzyć pliku!");
-    $data_to_write = get_data_to_write($new_password);
-    fwrite($admin_data_file, $data_to_write);
-    fclose($admin_data_file);
-    header("Location: ../index/index.php?page=profil");
-    exit();
+    if (is_password_valid($new_password)) {
+      $admin_data_file = fopen(ADMIN_DATA_FILE, "w") or die("Nie udało się otworzyć pliku!");
+      $data_to_write = get_data_to_write($new_password);
+      fwrite($admin_data_file, $data_to_write);
+      fclose($admin_data_file);
+      header("Location: ../index/index.php?page=profil");
+      exit();
+    } else {
+      echo "<h2 class='profil_form_errors'>Hasło powinno mieć minimum 5 znaków!</h2>";
+    }
   }
 
   function get_data_to_write($new_password) {
